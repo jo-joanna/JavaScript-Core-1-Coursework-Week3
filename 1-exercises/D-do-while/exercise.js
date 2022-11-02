f067e8ff6a3ee59384c8312ae05345487a12805a
jo-joanna <b.joanna.uk@gmail.com>
2022-11-02 02:33:23 +0100

    Update exercise.js

--- 1-exercises/D-do-while/exercise.js
+++ 1-exercises/D-do-while/exercise.js
@@ -1,15 +1,32 @@
 /*
-    Sometimes when using loops, we'll want to execute the body of the loop at least once. We can make sure this happens by using a do-while loop.
+    Sometimes when using loops, we'll want to execute the body of the loop at least once. We can make sure 
+    this happens by using a do-while loop.
         - If the condition in a while loop is initially false, the body of the loop will never execute
-        - But in a do-while loop, because the condition is checked after the body, we know that it will always execute at least once
+        - But in a do-while loop, because the condition is checked after the body, we know that
+         it will always execute at least once
     
     Using a do-while loop, write a function which returns the sum of the first n even numbers (starting from 0)
 */
 
 function evenNumbersSum(n) {
     // TODO
+    let sum=0;
+    let i=0;
+    let count=0;
+    do {
+        count += 1;
+        i+=1;
+        if (i%2 === 0){
+            sum += i;
+            if ( count === n) {
+            }
+            
+           }
+          }
+        while (count<=n);
+        return sum; 
 }
 
 console.log(evenNumbersSum(3)); // should output 6
 console.log(evenNumbersSum(0)); // should output 0
-console.log(evenNumbersSum(10)); // should output 90
+console.log(evenNumbersSum(10)); // should output 90 ??? should be 30
