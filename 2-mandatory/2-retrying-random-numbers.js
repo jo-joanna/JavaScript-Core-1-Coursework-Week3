0f8392cf63a98343d2bb7055263bc7944b385b45
jo-joanna <b.joanna.uk@gmail.com>
2022-11-04 00:19:41 +0100

    declare num, move return

--- 2-mandatory/2-retrying-random-numbers.js
+++ 2-mandatory/2-retrying-random-numbers.js
@@ -5,27 +5,28 @@
 
 // This function shouldn't be changed
 function generateRandomNumber() {
-    console.log("Generating number...");
-    return Math.round(Math.random() * 100);
+  console.log("Generating number...");
+  return Math.round(Math.random() * 100);
 }
 
 function getRandomNumberGreaterThan50() {
-    // TODO - implement using a do-while loop
-    do {
-        num = generateRandomNumber();
-        return num;
-    }
-    while (num > 50);
+  // TODO - implement using a do-while loop
+  let num;
+  do {
+    num = generateRandomNumber();
+
+  } while (num > 50);
+    return num;
 }
 
-getRandomNumberGreaterThan50()
+getRandomNumberGreaterThan50();
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("Returned value should always be greater than 50", () => {
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
 });
